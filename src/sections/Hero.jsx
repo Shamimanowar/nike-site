308afccf81cc8ebec6946102b888aea3dd891b16
shamim-anowar <shamim.hire@gmail.com>
2025-01-11 11:51:27 +0600

    complete hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,80 @@
-const Hero = () => {
-  return (
-    <div>Hero</div>
-  )
-}
+import { useState } from "react";
 
-export default Hero
+import Button from "../components/Button";
+import { arrowRight } from "../assets/icons";
+import { statistics, shoes } from "../constants";
+import { bigShoe1 } from "../assets/images";
+import ShowCard from "../components/ShowCard";
+
+const Hero = () => {
+  const [bigShoeImage, setBigShoeImage] = useState(bigShoe1);
+
+  return (
+    <section
+      id="home"
+      className="flex flex-col xl:flex-row justify-center min-h-screen gap-10 max-container w-full"
+    >
+      <div className="relative w-full xl:w-2/5 flex flex-col justify-center items-start max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer collections
+        </p>
+
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h1>
+
+        <p className="font-montserrat text-slate-gray text-xl leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+
+        <Button label="Shop Now" iconURL={arrowRight} />
+
+        <div className="flex justify-start items-start flex-wrap w-full gap-16 mt-20">
+          {statistics.map((stat, index) => (
+            <div
+              key={index}
+              className="flex flex-col justify-start align-center gap-1"
+            >
+              <p className="text-4xl font-bold font-palanquin">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Second Section */}
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero">
+        <img
+          src={bigShoeImage}
+          alt="show collection"
+          width={610}
+          height={502}
+          className="object-contain relative z-10"
+        />
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe, index) => {
+            return (
+              <div key={index}>
+                <ShowCard
+                  imgURL={shoe}
+                  changeBigShoeImage={setBigShoeImage}
+                  bigShoeImage={bigShoeImage}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
