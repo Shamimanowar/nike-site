fe2e514be20258cbb69215f720d523c9f33efe77
shamim_anowar <shamim.hire@gmail.com>
2025-01-11 18:05:15 +0600

    in-progress: work on popular products

---
+++ src/components/PopularProductCard.jsx
@@ -0,0 +1,36 @@
+import { star } from "../assets/icons";
+
+const PopularProductCard = ({ product }) => {
+  return (
+    <div>
+      <div className="flex-col gap-16 flex-1 shrink-1">
+        <img
+          src={product.imgURL}
+          alt={product.name}
+          width={282}
+          height={282}
+          className=""
+        />
+        <div className="flex flex-row mt-4 text-slate-gray gap-2.5">
+          <img
+            src={star}
+            alt="review icon"
+            height={24}
+            width={24}
+            className="object-contain text-coral-red"
+          />
+          <p className="text-xl font-montserrat">(4.5)</p>
+        </div>
+
+        <h3 className="text-2xl font-semibold font-montserrat mt-4">
+          {product.name}
+        </h3>
+        <p className="text-2xl leading-normal font-semibold font-montserrat text-coral-red mt-4">
+          {product.price}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PopularProductCard;
--- src/sections/PopularProduct.jsx
+++ src/sections/PopularProduct.jsx
@@ -1,7 +1,28 @@
+import { products } from "../constants";
+import PopularProductCard from "../components/PopularProductCard";
+
 const PopularProducts = () => {
   return (
-    <div>PopularProducts</div>
-  )
-}
+    <section>
+      <div className="">
+        <h1 className="text-4xl font-bold font-palanquin">
+          Our <span className="text-coral-red inline-block">Popular</span>{" "}
+          Products
+        </h1>
 
-export default PopularProducts
+        <p className="mt-2 text-slate-gray font-montserrat lg:max-w-lg">
+          Experience top-notch quality and style with our sought-after
+          selections. Discover a world of comfort, design, and value
+        </p>
+
+        <div className="mt-10 flex lex-row gap-10 flex-1 flex-wrap">
+          {products.map((product, index) => (
+            <PopularProductCard key={index} product={product} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PopularProducts;
