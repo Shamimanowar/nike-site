424a3e67d1d84c0ec6a6526c098206b1bcde5cb8
shamim-anowar <shamim.hire@gmail.com>
2025-01-18 11:18:23 +0600

    feat: add subscribe portion

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,7 +1,27 @@
+import Button from "../components/Button";
+
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
-  )
-}
+    <section
+      id="contact-us"
+      className="max-container flex flex-row justify-between max-lg:flex-col gap-10 items-center"
+    >
+      <h1 className="text-4xl w-full lg:max-w-md font-bold font-palanquin leading-[68px]">
+        Sign Up for <span className="text-coral-red">Updates</span> & Newsletter
+      </h1>
 
-export default Subscribe
+      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full h-17">
+        <input
+          type="text"
+          className="input"
+          placeholder="shamim.hire@gmail.com"
+        />
+        <div className="flex max-sm:justify-end items-center max-sm:w-full">
+          <Button label={"Sign Up"} fullWidth />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Subscribe;
