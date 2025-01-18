f6c7696f1dd9fd2d0d33f33a5088700a8b808d21
shamim-anowar <shamim.hire@gmail.com>
2025-01-18 11:16:34 +0600

    style: align button and padding

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Button = ({label, iconURL}) => {
+const Button = ({ label, iconURL, fullWidth }) => {
   return (
     <button
-    className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red ${
+        fullWidth && "w-full"
+      }`}
     >
-        {label}
-    
-        <img 
-        src={iconURL} 
-        alt='arrow right icon'
-        className='ml-2 rounded-full w-5 h-5'
-        />
-    </button>
-  )
-}
+      {label}
 
-export default Button
+      {iconURL && (
+        <img
+          src={iconURL}
+          alt="arrow right icon"
+          className="ml-2 rounded-full w-5 h-5"
+        />
+      )}
+    </button>
+  );
+};
+
+export default Button;
